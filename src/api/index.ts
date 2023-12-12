ab88618d558d75a6f5574b0163151b146d7a5457
masud-rahman-mvn <masudur.rahman@moveon.com.bd>
2023-12-12 10:37:27 +0600

    update: folder

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,13 +3,10 @@ import { ConfigModule } from "@medusajs/types"
 import getConfigFile from "@medusajs/utils/dist/common/get-config-file"
 import cors from "cors"
 import { Router } from "express"
-import { getStripePayments } from "../controllers/get-payments"
-import hooks from "./hooks"
+
 
 export default (rootDirectory) => {
   const app = Router()
-
-  hooks(app)
 
   const { configModule } = getConfigFile<ConfigModule>(
     rootDirectory,
@@ -27,8 +24,7 @@ export default (rootDirectory) => {
     authenticate()
   )
   app.get(`/admin/orders/stripe-payments/:order_id`, async (req, res) => {
-    const payments = await getStripePayments(req)
-    res.json({ payments })
+    // res.json({ payments })
   })
 
   return app
