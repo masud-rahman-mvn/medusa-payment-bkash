1ee74645505aea9872fa3c5e6f3693a5194c07c0
masud-rahman-mvn <masudur.rahman@moveon.com.bd>
2023-12-12 11:16:02 +0600

    done: compile and link with backend plugin

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,31 +1,34 @@
-import { authenticate } from "@medusajs/medusa"
-import { ConfigModule } from "@medusajs/types"
-import getConfigFile from "@medusajs/utils/dist/common/get-config-file"
-import cors from "cors"
-import { Router } from "express"
-
+import { authenticate } from "@medusajs/medusa";
+import { ConfigModule } from "@medusajs/types";
+import getConfigFile from "@medusajs/utils/dist/common/get-config-file";
+import cors from "cors";
+import { Router } from "express";
 
 export default (rootDirectory) => {
-  const app = Router()
+  const app = Router();
 
   const { configModule } = getConfigFile<ConfigModule>(
     rootDirectory,
     "medusa-config"
-  )
+  );
 
   const corsOptions = {
     origin: configModule?.projectConfig?.admin_cors?.split(","),
     credentials: true,
-  }
+  };
 
   app.use(
     `/admin/orders/stripe-payments/:order_id`,
     cors(corsOptions),
     authenticate()
-  )
+  );
   app.get(`/admin/orders/stripe-payments/:order_id`, async (req, res) => {
     // res.json({ payments })
-  })
+  });
+  app.get(`/test`, async (req, res) => {
+    console.log("test");
+    res.json({ message: "test" });
+  });
 
-  return app
-}
+  return app;
+};
